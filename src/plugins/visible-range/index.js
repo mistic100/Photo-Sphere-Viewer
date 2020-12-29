099a7122253995938cf272ca2112f596898b7d1a
Damien Sorel <contact@git.strangeplanet.fr>
2020-12-29 15:19:19 +0100

    Update src/plugins/visible-range/index.js

--- src/plugins/visible-range/index.js
+++ src/plugins/visible-range/index.js
@@ -118,7 +118,7 @@ export default class VisibleRangePlugin extends AbstractPlugin {
       // eslint-disable-next-line no-param-reassign
       range = null;
     }
-    // longitude range is between 0 (center of panorama) and 2*PI
+    // longitude range is between 0 and 2*PI
     if (range) {
       this.config.longitudeRange = range.map(angle => utils.parseAngle(angle));
     }
